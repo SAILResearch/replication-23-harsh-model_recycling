d15c7bf5c197dcacb708d81401ae62e052eb1fb1
Harsh Patel <pharsh58@gmail.com>
2024-02-01 13:39:34 -0500

    add lr and nn models

---
+++ ecoselekt/dnn/deepjit.py
@@ -0,0 +1,330 @@
+import datetime
+import os
+from collections import Counter
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from matplotlib import pyplot
+from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.model_selection import GridSearchCV
+from skorch import NeuralNetClassifier
+from skorch.helper import SliceDict
+from tqdm import tqdm
+
+import ecoselekt.utils as utils
+from ecoselekt.settings import settings
+
+torch.manual_seed(settings.RANDOM_SEED)
+
+
+class DeepJITExtended(nn.Module):
+    def __init__(self, vocab_code):
+        super(DeepJITExtended, self).__init__()
+
+        V_code = vocab_code
+        Dim = settings.EMBEDDING_DIM
+        Class = settings.CLASS_NUM
+
+        Ci = 1  # input of convolutional layer
+        Co = settings.NUM_FILTERS  # output of convolutional layer
+        Ks = settings.FILTER_SIZES  # kernel sizes
+
+        # CNN-2D for commit code
+        self.embed_code = nn.Embedding(V_code, Dim)
+        self.convs_code_line = nn.ModuleList([nn.Conv2d(Ci, Co, (K, Dim)) for K in Ks])
+        self.convs_code_file = nn.ModuleList([nn.Conv2d(Ci, Co, (K, Co * len(Ks))) for K in Ks])
+
+        # other information
+        self.dropout = nn.Dropout(settings.DROP_OUT)
+        # commits metrics are added to the code representation with 12 features
+        self.fc1 = nn.Linear((len(Ks) * Co) + 12, settings.HIDDEN_UNITS)  # hidden units
+        self.fc2 = nn.Linear(settings.HIDDEN_UNITS, Class)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward_msg(self, x, convs):
+        # note that we can use this function for commit code line to get the information of the line
+        x = x.unsqueeze(1)  # (N, Ci, W, D)
+        x = [F.relu(conv(x)).squeeze(3) for conv in convs]  # [(N, Co, W), ...]*len(Ks)
+        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N, Co), ...]*len(Ks)
+        x = torch.cat(x, 1)
+        return x
+
+    def forward_code(self, x, convs_line, convs_hunks):
+        n_batch, n_file = x.shape[0], x.shape[1]
+        x = x.reshape(n_batch * n_file, x.shape[2], x.shape[3])
+
+        # apply cnn 2d for each line in a commit code
+        x = self.forward_msg(x=x, convs=convs_line)
+
+        # apply cnn 2d for each file in a commit code
+        x = x.reshape(n_batch, n_file, settings.NUM_FILTERS * len(settings.FILTER_SIZES))
+        x = self.forward_msg(x=x, convs=convs_hunks)
+        return x
+
+    def forward(self, x_metrics, x_code):
+        x_code = self.embed_code(x_code)
+        x_code = self.forward_code(x_code, self.convs_code_line, self.convs_code_file)
+
+        x_commit = torch.cat((x_metrics, x_code), 1)
+        x_commit = self.dropout(x_commit)
+        out = self.fc1(x_commit)
+        out = F.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out).squeeze(1)
+        return out
+
+
+def tune_model(train_data, valid_data):
+    # !When using this make DeepJITExtended init accept keyword arguments
+    # TODO: add params
+    params = {
+        "module__num_filters": [16, 32, 64, 128],
+        "module__filter_sizes": [[1, 2, 3]],
+        "optimizer__lr": [1e-5],
+        "module__embedding_dim": [16, 32, 64, 128],
+        "module__dropout_keep_prob": [0.5],
+        "module__hidden_units": [512],
+    }
+
+    cc2ftr, data_pad_msg, data_pad_code, data_labels, dict_msg, dict_code = train_data
+    valid_cc2ftr, valid_data_pad_msg, valid_data_pad_code, valid_data_labels, _, _ = valid_data
+
+    # combine data
+    cc2ftr = torch.tensor(cc2ftr).long()
+    data_pad_msg = torch.tensor(data_pad_msg).long()
+    data_pad_code = torch.tensor(data_pad_code).long()
+    data_labels = torch.tensor(data_labels).float()
+
+    valid_cc2ftr = torch.tensor(valid_cc2ftr).long()
+    valid_data_pad_msg = torch.tensor(valid_data_pad_msg).long()
+    valid_data_pad_code = torch.tensor(valid_data_pad_code).long()
+    valid_data_labels = torch.tensor(valid_data_labels).float()
+
+    cc2ftr = torch.cat((cc2ftr, valid_cc2ftr), 0)
+    data_pad_msg = torch.cat((data_pad_msg, valid_data_pad_msg), 0)
+    data_pad_code = torch.cat((data_pad_code, valid_data_pad_code), 0)
+    data_labels = torch.cat((data_labels, valid_data_labels), 0)
+
+    params["module__vocab_msg"], params["module__vocab_code"] = [len(dict_msg)], [len(dict_code)]
+    params["module__embedding_ftr"] = [cc2ftr.shape[1]]
+
+    if len(data_labels.shape) == 1:
+        params["module__class_num"] = [1]
+    else:
+        params["module__class_num"] = [data_labels.shape[1]]
+
+    # create model with skorch
+    model = NeuralNetClassifier(
+        DeepJITExtended,
+        criterion=nn.BCELoss,
+        optimizer=torch.optim.Adam,
+        max_epochs=50,
+        batch_size=64,
+        verbose=False,
+    )
+
+    grid = GridSearchCV(estimator=model, param_grid=params, n_jobs=-1, cv=3, verbose=2)
+    print(cc2ftr.shape, data_pad_msg.shape, data_pad_code.shape, data_labels.shape)
+    X_dict = SliceDict(
+        ftr=cc2ftr,
+        msg=data_pad_msg,
+        code=data_pad_code,
+    )
+
+    grid_result = grid.fit(X_dict, data_labels)
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_["mean_test_score"]
+    stds = grid_result.cv_results_["std_test_score"]
+    params = grid_result.cv_results_["params"]
+
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+def train_model(train_data, valid_data, project_name, window):
+    # TODO: add params
+    params = {
+        "num_filters": 16,
+        "filter_sizes": "1, 2, 3",
+        "l2_reg_lambda": 1e-5,
+        "num_epochs": 50,
+        "embedding_dim": 16,
+        "no_cuda": False,
+        "dropout_keep_prob": 0.5,
+        "hidden_units": 512,
+        "batch_size": 64,
+    }
+
+    cc2ftr, data_pad_msg, data_pad_code, data_labels, dict_msg, dict_code, commit_ids = train_data
+    (
+        valid_cc2ftr,
+        valid_data_pad_msg,
+        valid_data_pad_code,
+        valid_data_labels,
+        _,
+        _,
+        valid_commit_ids,
+    ) = valid_data
+
+    # set up parameters
+    params["cuda"] = (not params["no_cuda"]) and torch.cuda.is_available()
+    del params["no_cuda"]
+    params["filter_sizes"] = [int(k) for k in params["filter_sizes"].split(",")]
+
+    params["vocab_msg"], params["vocab_code"] = len(dict_msg), len(dict_code)
+    params["embedding_ftr"] = cc2ftr.shape[1]
+
+    if len(data_labels.shape) == 1:
+        params["class_num"] = 1
+    else:
+        params["class_num"] = data_labels.shape[1]
+    params["device"] = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # create and train the defect model
+    model = DeepJITExtended(args=params)
+    if torch.cuda.is_available():
+        model = model.cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr=params["l2_reg_lambda"])
+
+    criterion = nn.BCELoss()
+    for epoch in range(1, params["num_epochs"] + 1):
+        total_loss = 0
+        # building batches for training model
+        batches = utils.mini_batches_update_DExtended(
+            X_ftr=cc2ftr,
+            X_msg=data_pad_msg,
+            X_code=data_pad_code,
+            Y=data_labels,
+            mini_batch_size=params["batch_size"],
+        )
+        model.train()
+        for i, (batch) in enumerate(tqdm(batches)):
+            ftr, pad_msg, pad_code, labels = batch
+            if torch.cuda.is_available():
+                ftr = torch.tensor(ftr).cuda()
+                pad_msg, pad_code, labels = (
+                    torch.tensor(pad_msg).cuda(),
+                    torch.tensor(pad_code).cuda(),
+                    torch.cuda.FloatTensor(labels),
+                )
+            else:
+                ftr = torch.tensor(ftr).long()
+                pad_msg, pad_code, labels = (
+                    torch.tensor(pad_msg).long(),
+                    torch.tensor(pad_code).long(),
+                    torch.tensor(labels).float(),
+                )
+
+            optimizer.zero_grad()
+            predict = model.forward(ftr, pad_msg, pad_code)
+            loss = criterion(predict, labels)
+            total_loss += loss
+            loss.backward()
+            optimizer.step()
+        # add validation loss
+        batches = utils.mini_batches_update_DExtended(
+            X_ftr=valid_cc2ftr,
+            X_msg=valid_data_pad_msg,
+            X_code=valid_data_pad_code,
+            Y=valid_data_labels,
+            mini_batch_size=8,
+        )
+        valid_loss = 0
+        model.eval()
+        for i, (batch) in enumerate(tqdm(batches)):
+            ftr, pad_msg, pad_code, labels = batch
+            ftr = torch.tensor(ftr).long()
+            pad_msg, pad_code, labels = (
+                torch.tensor(pad_msg).long(),
+                torch.tensor(pad_code).long(),
+                torch.tensor(labels).float(),
+            )
+
+            predict = model.forward(ftr, pad_msg, pad_code)
+            loss = criterion(predict, labels)
+            valid_loss += loss
+
+        print(
+            "Epoch %i / %i -- Total loss: %f, Val loss: %f"
+            % (epoch, params["num_epochs"], total_loss, valid_loss)
+        )
+
+    torch.save(model.state_dict(), settings.MODELS_DIR / f"{project_name}_w{window}_dnn.pt")
+
+
+def evaluation_model(data, project_name, window):
+    # TODO: add params
+    params = {
+        "num_filters": 16,
+        "filter_sizes": "1, 2, 3",
+        "embedding_dim": 16,
+        "no_cuda": False,
+        "dropout_keep_prob": 0.5,
+        "hidden_units": 512,
+        "batch_size": 64,
+    }
+    cc2ftr, pad_msg, pad_code, all_labels, dict_msg, dict_code, commit_ids = data
+    batches = utils.mini_batches_DExtended(
+        X_ftr=cc2ftr,
+        X_msg=pad_msg,
+        X_code=pad_code,
+        Y=all_labels,
+        mini_batch_size=params["batch_size"],
+    )
+
+    params["cuda"] = (not params["no_cuda"]) and torch.cuda.is_available()
+    del params["no_cuda"]
+    params["filter_sizes"] = [int(k) for k in params["filter_sizes"].split(",")]
+
+    params["vocab_msg"], params["vocab_code"] = len(dict_msg), len(dict_code)
+    params["embedding_ftr"] = cc2ftr.shape[1]
+
+    if len(all_labels.shape) == 1:
+        params["class_num"] = 1
+    else:
+        params["class_num"] = all_labels.shape[1]
+    params["device"] = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = DeepJITExtended(args=params)
+    if torch.cuda.is_available():
+        model = model.cuda()
+    model.load_state_dict(torch.load(settings.MODELS_DIR / f"{project_name}_w{window}_dnn.pt"))
+
+    model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+    with torch.no_grad():
+        all_predict, all_label = list(), list()
+        for i, (batch) in enumerate(tqdm(batches)):
+            ftr, pad_msg, pad_code, label = batch
+            if torch.cuda.is_available():
+                ftr = torch.tensor(ftr).cuda()
+                pad_msg, pad_code, labels = (
+                    torch.tensor(pad_msg).cuda(),
+                    torch.tensor(pad_code).cuda(),
+                    torch.cuda.FloatTensor(label),
+                )
+            else:
+                ftr = torch.tensor(ftr).long()
+                pad_msg, pad_code, labels = (
+                    torch.tensor(pad_msg).long(),
+                    torch.tensor(pad_code).long(),
+                    torch.tensor(label).float(),
+                )
+            if torch.cuda.is_available():
+                predict = model.forward(ftr, pad_msg, pad_code)
+                predict = predict.cpu().detach().numpy().tolist()
+            else:
+                predict = model.forward(ftr, pad_msg, pad_code)
+                predict = predict.detach().numpy().tolist()
+            all_predict += predict
+            all_label += labels.tolist()
+
+    all_pred_label = np.array(all_predict) > 0.5
+    print("distribution of predicted labels:", Counter(all_pred_label))
+    print("distribution of actual labels:", Counter(all_label))
+
+    auc_score = roc_auc_score(y_true=all_label, y_score=all_predict)
+    print("Test data -- AUC score:", auc_score)
+
+    return np.array(all_predict), all_pred_label, commit_ids, np.array(all_label)
---
+++ ecoselekt/train_selekt_nn.py
@@ -0,0 +1,336 @@
+import pickle
+import time
+from collections import Counter
+
+import numpy as np
+import pandas as pd
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import make_pipeline as make_pipeline_imb
+from scipy.optimize import differential_evolution
+from scipy.stats import wilcoxon
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+
+import ecoselekt.utils as utils
+from ecoselekt.log_util import get_logger
+from ecoselekt.settings import settings
+
+_LOGGER = get_logger()
+
+
+def objective_func(k, train_feature, train_label, valid_feature, valid_label):
+    clf = make_pipeline_imb(
+        SMOTE(
+            random_state=settings.RANDOM_SEED,
+            n_jobs=-1,
+            k_neighbors=int(np.round(k)),
+        ),
+        LogisticRegression(
+            random_state=settings.RANDOM_SEED,
+        ),
+    )
+
+    clf.fit(train_feature, train_label)
+
+    prob = clf.predict_proba(valid_feature)[:, 1]
+    auc = roc_auc_score(valid_label, prob)
+
+    return -auc
+
+
+def save_selekt_model(project_name="activemq"):
+    _LOGGER.info(f"Starting selekt model training for {project_name}")
+    start = time.time()
+    # get test train code changes
+    (
+        all_code,
+        all_commit,
+        all_label,
+    ) = utils.prep_apachejit_data(project_name)
+    _LOGGER.info(f"Loaded code changes in {time.time() - start}")
+
+    # get commit metrics
+    start = time.time()
+    commit_metrics = utils.get_apachejit_commit_metrics(project_name)
+    commit_metrics = commit_metrics.drop(
+        ["fix", "year", "buggy"],
+        axis=1,
+    )
+    commit_metrics = commit_metrics.fillna(value=0)
+    _LOGGER.info(f"Loaded commit metrics in {time.time() - start}")
+
+    # combine train and test code change without label because
+    # label is model version which we add later
+    df = pd.DataFrame(
+        {
+            "commit_id": all_commit,
+            "code": all_code,
+        }
+    )
+
+    # merge commit metrics to train code
+    df = pd.merge(df, commit_metrics, on="commit_id")
+    _LOGGER.info(f"Loaded total data size for project({project_name}): {df.shape}")
+
+    start = time.time()
+    # load sliding windows splits
+    with open(settings.DATA_DIR / f"{settings.EXP_ID}_{project_name}_windows.pkl", "rb") as f:
+        windows = pickle.load(f)
+
+    _LOGGER.info(
+        f"Project: {project_name} with {len(windows)} windows loaded in {time.time() - start}"
+    )
+
+    pred_result_df = pd.read_csv(
+        settings.DATA_DIR / f"{settings.EXP_ID}_{project_name}_pred_result_nn.csv"
+    )
+
+    selekt_train_df = pd.DataFrame(columns=["window", "commit_id", "model_version"])
+    selekt_eval_df = pd.DataFrame(columns=["window", "prob", "pred", "actual", "test_commit"])
+    # start after model history is built
+    # and ignore last few windows (`settings.TEST_SIZE`) since there is no test data if we use it
+    for i in range(settings.MODEL_HISTORY, len(windows) - settings.C_TEST_WINDOWS):
+        _LOGGER.info(f"Starting window {i} for {project_name}")
+
+        # filter out "unavailable at the window time" future test commits
+        split = pd.concat(
+            [
+                windows[j].iloc[-settings.SHIFT :]
+                for j in range(i - settings.MODEL_HISTORY, i + settings.F_TEST_WINDOWS)
+            ],
+            ignore_index=True,
+        )
+        if settings.TEST_SIZE % settings.SHIFT != 0:
+            split = pd.concat(
+                [
+                    split,
+                    windows[i + settings.F_TEST_WINDOWS][
+                        -settings.SHIFT : (settings.TEST_SIZE % settings.SHIFT) - settings.SHIFT
+                    ],
+                ],
+                ignore_index=True,
+            )
+
+        all_pred_dfs = []
+        # load all past model predictions including latest model prediction
+        for j in range(i - settings.MODEL_HISTORY, i + 1):
+            temp_df = pred_result_df[pred_result_df["window"] == j].copy()
+            temp_df.rename(columns={"test_commit": "commit_id"}, inplace=True)
+            temp_df.drop("window", axis=1, inplace=True)
+            # filter out commit ids that are not in the current window
+            temp_df = temp_df[temp_df["commit_id"].isin(split.commit_id)]
+            all_pred_dfs.append(temp_df)
+
+        pred_df = pd.concat(all_pred_dfs, ignore_index=True)
+        _LOGGER.info(f"Prediction df shape: {pred_df.shape}")
+
+        pred_df["error"] = abs(pred_df["actual"] - pred_df["prob"])
+
+        # use wilcoxon rank sum test to find similar model versions group
+        # and keep only one model version from each group
+        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.wilcoxon.html
+        og_model_versions = sorted(pred_df["model_version"].unique())
+        _LOGGER.info(f"Total model versions: {len(og_model_versions)}")
+        models_to_remove = []
+        # deduplicate and copy train_pred_df by commit_id keeping the row with the lowest error
+        temp_pred_df = (
+            pred_df.sort_values("error").drop_duplicates(subset="commit_id", keep="first").copy()
+        )
+        # get all model versions that are not in the same group as the any other model versions
+        _LOGGER.info(f"Filtering using wilcoxon rank sum test with alpha={settings.ALPHA}")
+        for model_version_a in range(len(og_model_versions) - 1):
+            if og_model_versions[model_version_a] in models_to_remove:
+                continue
+            for model_version_b in range(model_version_a + 1, len(og_model_versions)):
+                _, p = wilcoxon(
+                    pred_df[pred_df["model_version"] == og_model_versions[model_version_a]][
+                        "error"
+                    ].values,
+                    pred_df[pred_df["model_version"] == og_model_versions[model_version_b]][
+                        "error"
+                    ].values,
+                )
+                if p >= settings.ALPHA:
+                    # check frequncy of model version
+                    if (
+                        temp_pred_df[
+                            temp_pred_df["model_version"] == og_model_versions[model_version_a]
+                        ]["error"].count()
+                        > temp_pred_df[
+                            temp_pred_df["model_version"] == og_model_versions[model_version_b]
+                        ]["error"].count()
+                    ):
+                        models_to_remove.append(og_model_versions[model_version_b])
+                    else:
+                        models_to_remove.append(og_model_versions[model_version_a])
+                        break
+        # filter out model versions that are similar to other model versions
+        # and keep the latest model version
+        model_versions = set(og_model_versions) - set(models_to_remove)
+        if i not in model_versions:
+            model_versions.add(i)
+
+        pred_df = pred_df[pred_df["model_version"].isin(model_versions)]
+
+        _LOGGER.info(f"After wilcoxon prediction df shape: {pred_df.shape}")
+        _LOGGER.info(f"Model versions kept: {model_versions}")
+
+        with open(
+            settings.MODELS_DIR / f"{settings.EXP_ID}_{project_name}_w{i}_stat_models_nn.pkl", "wb"
+        ) as f:
+            f.write(pickle.dumps(model_versions))
+
+        # add models probabilities as features
+        for model_version in model_versions:
+            prob_df = (
+                pred_df[pred_df["model_version"] == model_version][["commit_id", "prob"]]
+                .drop_duplicates(subset="commit_id", keep="first")
+                .rename(columns={"prob": f"prob_{model_version}"})
+                .reset_index(drop=True)
+                .copy()
+            )
+            pred_df = pred_df.merge(prob_df, on="commit_id", how="left")
+
+        # deduplicate train_pred_df by commit_id
+        pred_df = pred_df.drop_duplicates(subset="commit_id", keep="first")
+        _LOGGER.info(f"After dedup prediction df shape: {pred_df.shape}")
+        # shuffle the dataframe
+        pred_df = pred_df.sample(frac=1, random_state=settings.RANDOM_SEED).reset_index(drop=True)
+        pred_df.drop("error", axis=1, inplace=True)
+        _LOGGER.info(f"Label distribution: {Counter(pred_df['model_version'])}")
+
+        # *[OUT]: save ecoselekt train data
+        selekt_train_df = pd.concat(
+            [
+                selekt_train_df,
+                pred_df[["commit_id", "model_version"]].assign(window=i),
+            ],
+            ignore_index=True,
+        )
+        selekt_train_df.to_csv(
+            settings.DATA_DIR / f"{settings.EXP_ID}_{project_name}_selekt_train_data.csv",
+            index=False,
+        )
+
+        pred_df = pd.merge(pred_df, df, on="commit_id", how="left")
+
+        # stratified random resampling to balance the dataset and reduce to `settings.WINDOW_SIZE`
+        pred_df = pred_df.groupby("actual").apply(
+            lambda x: x.sample(
+                n=min(settings.WINDOW_SIZE // 2, x.shape[0]),
+                random_state=settings.RANDOM_SEED,
+                replace=False,
+            )
+        )
+        _LOGGER.info(f"Train df shape: {pred_df.shape}")
+        _LOGGER.info(f"Label distribution: {Counter(pred_df['actual'])}")
+        # # train using current window
+        # train_split = windows[i].sample(
+        #     frac=1, random_state=settings.RANDOM_SEED, ignore_index=True
+        # )
+        # # keep current window commit ids
+        # pred_df = pred_df[pred_df["commit_id"].isin(train_split.commit_id)]
+
+        # !: uncomment to save pred_df for experiment
+        # pred_df.to_csv(
+        #     settings.DATA_DIR / f"{settings.EXP_ID}_{project_name}_w{i}_pred_train_exp.csv",
+        #     index=False,
+        # )
+
+        model_prob_features = [f"prob_{model_version}" for model_version in model_versions]
+
+        # train validation split
+        (
+            final_train_feature,
+            valid_feature,
+            final_train_label,
+            valid_label,
+        ) = train_test_split(
+            pred_df[model_prob_features].values,
+            pred_df.actual.values,
+            test_size=0.2,
+            random_state=settings.RANDOM_SEED,
+            stratify=pred_df.actual.values,
+        )
+
+        positive_label_count = np.sum(final_train_label)
+        bounds = [(1, positive_label_count - 1 if positive_label_count < 20 else 20)]
+        start = time.time()
+        result = differential_evolution(
+            objective_func,
+            bounds=bounds,
+            args=(
+                final_train_feature,
+                final_train_label,
+                valid_feature,
+                valid_label,
+            ),
+            popsize=10,
+            mutation=0.7,
+            recombination=0.3,
+            seed=0,
+        )
+        _LOGGER.info(f"Completed differential evolution for window {i} in {time.time() - start}")
+
+        clf = make_pipeline_imb(
+            SMOTE(
+                random_state=settings.RANDOM_SEED,
+                n_jobs=-1,
+                k_neighbors=int(np.round(result.x)),
+            ),
+            LogisticRegression(
+                random_state=settings.RANDOM_SEED,
+            ),
+        )
+
+        clf.fit(final_train_feature, final_train_label)
+        _LOGGER.info(f"Selekt model training finished in {time.time() - start}")
+
+        # evaluate model on train data
+        prob = clf.predict_proba(pred_df[model_prob_features].values)
+        pred = clf.predict(pred_df[model_prob_features].values)
+        _LOGGER.info(f"Validation score: {clf.score(valid_feature, valid_label)}")
+        _LOGGER.info(f"Train score: {clf.score(final_train_feature, final_train_label)}")
+
+        # *[OUT]: save results for evaluation
+        selekt_eval_df = pd.concat(
+            [
+                selekt_eval_df,
+                pd.DataFrame(
+                    {
+                        "window": i,
+                        "prob": prob.tolist(),
+                        "pred": pred,
+                        "actual": pred_df.actual.values.tolist(),
+                        "test_commit": pred_df.commit_id.values.tolist(),
+                    }
+                ),
+            ],
+            ignore_index=True,
+        )
+        selekt_eval_df.to_csv(
+            settings.DATA_DIR / (f"{settings.EXP_ID}_{project_name}_selekt_model_pred_nn.csv"),
+            index=False,
+        )
+
+        # *[OUT]: save selekt model
+        with open(
+            settings.MODELS_DIR / f"{settings.EXP_ID}_{project_name}_w{i}_selekt_model_nn.pkl", "wb"
+        ) as f:
+            pickle.dump(clf, f)
+        _LOGGER.info(f"Selekt model saved for {project_name} window {i}")
+
+
+def main():
+    settings.DATA_DIR.mkdir(parents=True, exist_ok=True)
+    settings.MODELS_DIR.mkdir(parents=True, exist_ok=True)
+
+    try:
+        for project_name in settings.PROJECTS:
+            _LOGGER.info(f"Starting {project_name}")
+            start = time.time()
+            save_selekt_model(project_name)
+            _LOGGER.info(f"Finished {project_name} in {time.time() - start}")
+    except Exception:
+        _LOGGER.exception("Unexpected error occurred.")
